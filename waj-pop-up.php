d08d5ab63f629c86c65c1be90c314d7a9e7eba90
Jaimeson Waugh <jaimeson@4cesi.com>
2018-11-21 12:58:37 -0800

    Init

---
+++ waj-pop-up.php
@@ -0,0 +1,12 @@
+<?php
+	/*
+	Plugin Name:  WAJ Pop Up
+	Plugin URI:   https://github.com/waughjai/copyright-year
+	Description:  Create pop-up windows that appear on the front page during certain times.
+	Version:      1.0.0
+	Author:       Jaimeson Waugh
+	Author URI:   https://www.jaimeson-waugh.com
+	License:      GPL2
+	License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+	Text Domain:  waj-pop-up
+	*/
