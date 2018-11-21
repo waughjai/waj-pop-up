8b0c0f12fa127ee1efcfb70a1328889457a800a6
Jaimeson Waugh <jaimeson@4cesi.com>
2018-11-21 13:33:45 -0800

    Add main pop-up content

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 ( function()
 {
-	console.log( "POPUP" );
+	var ID = 'thanksgiving-pop-up';
+	var div = document.createElement( 'div' );
+	div.setAttribute( 'class', ID );
+	div.setAttribute( 'id', ID );
+	div.innerHTML = '<h2>Lincoln South Food Hall will be closed all day this Thanksgiving</h2>' +
+		'<p>We wish you a Happy Thanksgiving!</p>';
+	document.body.appendChild( div );
 })();
