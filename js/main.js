d9fdaf43acb1c7b82e2b1fb34cbd2719938e5a19
Jaimeson Waugh <jaimeson@4cesi.com>
2018-11-21 13:57:06 -0800

    Style pop-up

--- js/main.js
+++ js/main.js
@@ -4,9 +4,10 @@
 	var div = document.createElement( 'div' );
 	div.setAttribute( 'class', ID );
 	div.setAttribute( 'id', ID );
-	div.innerHTML = '<div class="' + ID + '-box"><h2>Lincoln South Food Hall will be closed all day this Thanksgiving</h2>' +
+	div.innerHTML = '<div id="' + ID + '-box" class="' + ID + '-box"><h2>Lincoln South Food Hall will be closed all day this Thanksgiving</h2>' +
 		'<p>We wish you a Happy Thanksgiving!</p>';
 	document.body.appendChild( div );
 
-	$( '#' . ID ).click( function() { $( this ).fadeOut(); } );
+	$( '#' + ID ).click( function() { $( this ).fadeOut(); } );
+	$( '#' + ID + '-box' ).click( function() { $( '#' . ID ).fadeOut(); } );
 })();
