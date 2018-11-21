5f128d559d18a7c12f9729a49baebcac9b24aae9
Jaimeson Waugh <jaimeson@4cesi.com>
2018-11-21 14:31:47 -0800

    Make message only show at start of session

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 ( function()
 {
 	var ID = 'thanksgiving-pop-up';
+
+	if ( sessionStorage.getItem( "thanksgiving-already-seen-msg" ) === "true" )
+	{
+		$( '#' + ID ).hide();
+	}
+
 	$( '#' + ID ).click
 	(
 		function()
 		{
 			$( this ).fadeOut();
+			sessionStorage.setItem( "thanksgiving-already-seen-msg", "true" )
 		}
 	);
 })();
